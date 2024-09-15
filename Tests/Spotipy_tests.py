e1800c233ca627e0ee8bacfae212da3d11f3eac1
Priya Deshmukh <priyadeshm155@gmail.com>
2024-09-15 17:38:33 -0400

    Test 1 ,Git check

--- Tests/Spotipy_tests.py
+++ Tests/Spotipy_tests.py
@@ -7,3 +7,6 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="0e6ced6cd3
 results = sp.search(q='weezer', limit=20)
 for idx, track in enumerate(results['tracks']['items']):
     print(idx, track['name'])
+
+
+    print("tests")
